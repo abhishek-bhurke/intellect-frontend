6061054dbc48b39902d077f9150c4a13604a000d
abhishek-bhurke <abhishekbhurke25@gmail..com>
2025-05-22 23:51:51 +0530

    ui changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,6 +11,7 @@ export class HeaderComponent {
   activeSection: string = 'home'
   sections = ['home', 'about-us', 'services', 'contact-us'];
   isCollapse: boolean = false;
+  currentSection: any;
   constructor(private eRef: ElementRef) { }
   @HostListener('window:scroll', ['$event'])
   onScroll(event: any): void {
@@ -27,20 +28,18 @@ export class HeaderComponent {
   }
   highlightLink() {
     const sections = ['home', 'about-us', 'services', 'contact-us'];
-    let currentSection = '';
-
     sections.forEach(section => {
       const element = document.querySelector(`#${section}`);
       if (element) {
         const rect = element.getBoundingClientRect();
         const windowHeight = window.innerHeight;
         if (rect.top < windowHeight * 0.49 && rect.bottom > windowHeight * 0.51) {
-          currentSection = section;
+          this.currentSection = section;
         }
       }
     });
 
-    this.activeSection = currentSection;
+    this.activeSection = this.currentSection;
   }
   scrollToSection(sectionId: string) {
     const element = document.getElementById(sectionId);
